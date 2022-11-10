19c89f1d639f6e5171aa4c7ae9a800579746e7a5
Alex <>
2022-11-10 00:50:45 -0500

    run JoystickButton instead of custom for now

--- src/main/java/frc/robot/subsystems/TeleopDriver.java
+++ src/main/java/frc/robot/subsystems/TeleopDriver.java
@@ -1,13 +1,13 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.util.WController;
-import frc.robot.util.WJoystickButton;
 import frc.robot.util.WScheduler;
 
 public class TeleopDriver implements WController {
 
-    private WJoystickButton btnA, btnB;
+    private JoystickButton btnA, btnB;
     private final WScheduler wsc;
 
     public TeleopDriver(WScheduler wsc) {
@@ -16,8 +16,8 @@ public class TeleopDriver implements WController {
 
     public void initialize() {
         XboxController driver = new XboxController(0);
-        btnA = new WJoystickButton(driver, XboxController.Button.kA.value);
-        btnB = new WJoystickButton(driver, XboxController.Button.kB.value);
+        btnA = new JoystickButton(driver, XboxController.Button.kA.value);
+        btnB = new JoystickButton(driver, XboxController.Button.kB.value);
     }
 
     public void periodic() {
